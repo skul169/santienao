eb19f4860e1dcaaf7b473a57001ac31433167108
Nguyen Van Truong <truongnv@evolableasia.vn>
2017-08-16 10:45:00 +0700

    Update

--- fuel/app/views/eth/buy.php
+++ fuel/app/views/eth/buy.php
@@ -1,7 +1,7 @@
 <page-header class="page-header-title ng-scope">
     <h1 class="ng-binding"><?php echo \Lang::get('fields.buy_eth'); ?></h1>
 </page-header>
-<form action="" class="form-horizontal ng-pristine ng-scope ng-valid-min ng-invalid ng-invalid-required" name="sellForm" method="post">
+<form action="" class="form-horizontal ng-pristine ng-scope ng-valid-min ng-invalid ng-invalid-required" name="sellForm" method="post" id="buy-form">
     <div class="panel panel-default">
         <div class="panel-heading">
             <span class="text-muted"><?php echo \Lang::get('fields.vietcombank'); ?><br><?php echo \Lang::get('fields.vietcomabank_note'); ?></span>
@@ -11,7 +11,7 @@
                 <label class="col-md-4 control-label"><?php echo \Lang::get('fields.eth_want_to_buy'); ?></label>
                 <div class="col-md-8">
                     <div class="input-group">
-                        <input class="form-control" min="0.01" name="quantity" placeholder="e.g.: 10" required="required" step="any" type="number" style="">
+                        <input class="form-control" min="0.01" name="quantity" placeholder="e.g.: 10" required="required" step="any" type="number" style="" id="coin_number">
                         <span class="input-group-addon">ETH</span>
                     </div>
                     <div class="help-block">Số lượng tối thiểu là 0.01 BTC</div>
@@ -30,16 +30,52 @@
             <div class="form-group required has-feedback account-number">
                 <label class="col-md-4 control-label"><?php echo \Lang::get('fields.your_eth_address'); ?></label>
                 <div class="col-md-8">
-                    <input class="form-control" name="accountNumber" placeholder="Vd: 1JyW3CWQRZtX98NVMvqTBu6SKt75BuxZVg" required type="text">
+                    <input class="form-control" name="accountNumber" placeholder="Eg: 1JyW3CWQRZtX98NVMvqTBu6SKt75BuxZVg" required type="text" id="accountNumber">
+                    <span class="has-error" style="display: none;" id="accountNumber-span">Số tài khoản không chính xác</span>
                 </div>
             </div>
         </div>
         <div class="panel-footer text-right">
-                          <span ng-show="vm.submitting" style="display: none;">
-                            <i class="fa-2x ng-scope" translate="PROCESSING"><?php echo \Lang::get('fields.processing'); ?></i>
-                            <i class="fa fa-spinner fa-spin fa-2x"></i>
-                          </span>
-            <button class="btn" type="submit"><?php echo \Lang::get('fields.next'); ?></button>
+            <span style="display: none;" id="processing">
+                <i class="fa-2x ng-scope"><?php echo \Lang::get('fields.processing'); ?></i>
+            </span>
+            <button class="btn" type="button" id="submit-btn"><?php echo \Lang::get('fields.next'); ?></button>
         </div>
     </div>
 </form>
+<script>
+    var isAddress = function (address) {
+        if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
+            // check if it has the basic requirements of an address
+            return false;
+        } else if (/^(0x)?[0-9a-f]{40}$/.test(address) || /^(0x)?[0-9A-F]{40}$/.test(address)) {
+            // If it's all small caps or all all caps, return true
+            return true;
+        } else {
+            // Otherwise check each case
+            return true;
+        }
+    };
+
+    $(document).ready(function () {
+        $("#submit-btn").click(function () {
+            var walletAddress = $("#accountNumber").val();
+            if (walletAddress == '' || !isAddress(walletAddress)) {
+                $("#accountNumber-span").show();
+                return false;
+            } else {
+                $("#accountNumber-span").hide();
+            }
+
+            if ($("#coin_number").val() > 0) {
+
+            } else {
+                return false;
+            }
+
+            $("#processing").show();
+            $("#submit-btn").hide();
+            $("#buy-form").submit();
+        });
+    });
+</script>
