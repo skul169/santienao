9006062a4e91866278f1e7417416d56da36384a4
truongnv@evolableasia.vn <Nguyen Van Truong>
2017-08-21 21:59:44 +0700

    update

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -210,7 +210,7 @@
     (function(){
         var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
         s1.async=true;
-        s1.src='https://embed.tawk.to/5992ff061b1bed47ceb04bad/default';
+        s1.src='https://embed.tawk.to/599af4ab1b1bed47ceb05cfe/default';
         s1.charset='UTF-8';
         s1.setAttribute('crossorigin','*');
         s0.parentNode.insertBefore(s1,s0);
