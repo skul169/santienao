22acd897692428b20f71737850d11cde8b9bb0f8
Nguyen Van Truong <truongnv@evolableasia.vn>
2017-08-22 10:33:41 +0700

    update

--- fuel/app/views/welcome/index.php
+++ fuel/app/views/welcome/index.php
@@ -1,15 +1,31 @@
 <div class="row">
-    <div class="col-md-6">
-        <?php echo \Lang::get('fields.you_want'); ?> <label style="font-size: 22px; font-weight: bold; color: #0000BB;"><?php echo \Lang::get('fields.buy'); ?></label>
-        <div class="row">
-            <a class="btn btn-primary btn-lg" href="eth/buy" role="button"><?php echo number_format($price['buy']); ?> VND</a>
-        </div>
-    </div>
-    <div class="col-md-6">
-        <?php echo \Lang::get('fields.you_want'); ?> <label style="font-size: 22px; font-weight: bold; color: #953b39"><?php echo \Lang::get('fields.sell'); ?></label>
-        <div class="row">
-            <a class="btn btn-primary btn-lg" href="eth/sell" role="button"><?php echo number_format($price['sell']); ?> VND</a>
-        </div>
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <section class="title_center_main" style="text-align: center;">
+            <h1 style="margin-top: 15px; text-transform: uppercase; font-family: UTM_HelvetIns;">Chợ mua bán Ethereum</h1>
+            <p style="font-size: 17px; margin: 14px 0 25px;">Nơi mua bán Ethereum AN TOÀN và NHANH CHÓNG</p>
+        </section>
+        <section id="on_out" style="margin-right: -15px; margin-left: -15px;">
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                <article style="text-align: center;">
+                    <h4 style="text-transform: uppercase; font-weight: bold; font-size: 15px;"><?php echo \Lang::get('fields.you_want'); ?> <?php echo \Lang::get('fields.buy'); ?>:</h4>
+                    <h6 id="buy-price-text" class="green txt-price-buy" style="font-size: 20px; font-weight: bold; margin: 10px 0 20px; color: #449d44;"><?php echo number_format($price['buy']); ?> VNĐ</h6>
+                    <div class="text-center" style="text-align: center;">
+                        <a style="border-radius: 0; padding: 10px 25px; font-weight: bold; font-size: 16px; background-color: #5cb85c; border-color: #4cae4c; background-image: none;"
+                           class="btn btn-success" href="eth/buy">Mua ngay</a>
+                    </div>
+                </article>
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                <article style="text-align: center;">
+                    <h4 style="text-transform: uppercase; font-weight: bold; font-size: 15px;"><?php echo \Lang::get('fields.you_want'); ?> <?php echo \Lang::get('fields.sell'); ?>:</h4>
+                    <h6 id="sell-price-text" class="red txt-price-sell" style="font-size: 20px; font-weight: bold; margin: 10px 0 20px; color: #c9302c;"><?php echo number_format($price['sell']); ?> VNĐ</h6>
+                    <div class="text-center" style="text-align: center;">
+                        <a style="border-radius: 0; padding: 10px 25px; font-weight: bold; font-size: 16px; background-color: #d9534f; border-color: #d43f3a; background-image: none;"
+                            class="btn btn-danger" href="eth/sell">Bán ngay</a>
+                    </div>
+                </article>
+            </div>
+        </section>
     </div>
 </div>
 <hr>
