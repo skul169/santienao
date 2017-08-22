7728ea4cd3e409974c36962925f1ca0154542fe0
Nguyen Van Truong <truongnv@evolableasia.vn>
2017-08-22 15:59:00 +0700

    update logo

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -36,7 +36,7 @@
 <body>
 
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <a class="navbar-brand" href="#" style="color: #fff !important;">ETH</a>
+    <a class="navbar-brand" href="/" style="color: #fff !important;"><img src="/logo.jpg"></a>
     <button class="navbar-toggler pull-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
             aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
